f9cf3bf76d0ddc03f9aa8a1f488b37733304a583
Quan Du <quandu0324@gmail.com>
2022-11-04 15:39:18 +0700

    bot-ver 1.0

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -22,7 +22,7 @@ module.exports = (client) => {
     }
 
     const clientId = "1036484123763691520";
-    const guildID = "931602162428018688";
+    const guildID = "973782396774264923";
     const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
     try {
       console.log("Start refreshing application (/) commands.");
